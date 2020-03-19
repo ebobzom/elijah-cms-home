d5a29f01ed51d2bd2d64122b98af87a4bb3965c0
Elijah Bobzom <ebobzom@gmail.com>
2020-03-19 16:53:05 +0100

    add font awesome and reponsive design

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,60 @@
 <template>
   <div id="app">
     <NavBar />
-    <div class="product-display">
+    <div id="product-displayc">
         <CategoryBar />
         <TestPage />
     </div> 
     <ImgComponent />
+     <!-- <b-card >
+        <b-card-text>
+            <h6>ok</h6>
+            <a class="links" href="#">ok</a>
+        </b-card-text>
+    </b-card> -->
+    <div id="ft-container">
+      <!-- <Footer v-for="(containers, index) in urlData" :key="containers.id" :propsUrlList="containers" :titleContent="titleData[index]" /> -->
+      <b-card class="test">
+        <b-card-text>
+            <h6>ABOUT</h6>
+            <a href="#">Contact us</a>
+            <a href="#">How to shop on Jumia?</a>
+            <a href="#">Delivery options and timelines</a>
+            <a href="#">How to return a product on Jumia?</a>
+            <a href="#">Corporate and bulk purchase</a>
+        </b-card-text>
+      </b-card>
+      <b-card class="test">
+        <b-card-text>
+            <h6>HELP</h6>
+            <a href="#">Contact us</a>
+            <a href="#">How to shop on Jumia?</a>
+            <a href="#">Delivery options and timelines</a>
+            <a href="#">How to return a product on Jumia?</a>
+            <a href="#">Corporate and bulk purchase</a>
+        </b-card-text>
+      </b-card>
+      <b-card class="test">
+        <b-card-text>
+            <h6>JUMIA</h6>
+            <a href="#">Contact us</a>
+            <a href="#">How to shop on Jumia?</a>
+            <a href="#">Delivery options and timelines</a>
+            <a href="#">How to return a product on Jumia?</a>
+            <a href="#">Corporate and bulk purchase</a>
+        </b-card-text>
+      </b-card>
+      <b-card class="test">
+        <b-card-text>
+            <h6> MONEY</h6>
+            <a href="#">Contact us</a>
+            <a href="#">How to shop on Jumia?</a>
+            <a href="#">Delivery options and timelines</a>
+            <a href="#">How to return a product on Jumia?</a>
+            <a href="#">Corporate and bulk purchase</a>
+        </b-card-text>
+      </b-card>
+    </div>
   </div>
 </template>
 
@@ -14,14 +63,33 @@ import NavBar from './components/NavBar';
 import CategoryBar from './components/CategoryBar';
 import ImgComponent from './components/ImgComponent';
 import TestPage from './components/TestPage';
+// import Footer from './components/FooterLinkComponent'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      titleData: ['LET US HELP YOU', 'ABOUT JUMIA'],
+      urlData: [
+        {
+          id: 1,
+          path: '#',
+          content: 'Help Center'
+        },
+        {
+          id: 2,
+          path: '#',
+          content: 'Contact us'
+        }
+      ]
+    }
+  },
   components: {
     NavBar,
     CategoryBar,
     ImgComponent,
-    TestPage
+    TestPage,
+    // Footer
   }
 }
 </script>
@@ -32,7 +100,20 @@ export default {
     max-width: 1260px;
   }
 
-  .product-display{
+  .test{
+    width: 25%;
+    background-color: rgb(240, 238, 238);
+    margin-top: 50px;
+    margin-bottom: 20px;
+    color: black;
+    display: inline-block;
+    border: none;
+  }
+  .test a{
+    color: rgb(99, 95, 95);
+  }
+
+  #product-displayc{
     margin: 0 auto;
     display: flex;
   }
@@ -42,8 +123,9 @@ export default {
       flex-direction: column;
     }
 
+ 
     .category{
-      min-width: 90%;
+      min-width: 80%;
       margin: 0 auto;
       text-align: center;
       margin-left: 19px;
---
+++ src/components/FooterLinkComponent.vue
@@ -0,0 +1,45 @@
+<template>
+
+    <b-card class="each-card">
+        <b-card-text>
+            <h6>{{titleContent}}</h6>
+            <a class="links" href="#">{{ propsUrlList.content }}</a>
+        </b-card-text>
+    </b-card>
+
+</template>
+
+<script>
+export default {
+    props: ['propsUrlList', 'titleContent']
+}
+</script>
+
+<style scoped>
+    h6{
+        font-size: 18px;
+        text-align: left;
+        padding: 0;
+        margin: 0;
+    }
+
+    .links{
+        margin: 0;
+        padding: 0;
+        width: 100px;
+        color: red;
+        text-align: left;
+        /* margin-right: 90px; */
+    }
+
+    .each-card{
+        width: 100px;
+        margin: 0 auto;
+        text-align: center;
+        /* background-color: #323232; */
+    }
+
+    .footer-link{
+        border: 2px solid green;
+    }
+</style>
