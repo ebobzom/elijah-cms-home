26bc1e89315c4576462756717e85bfa571321a81
Elijah Bobzom <ebobzom@gmail.com>
2020-03-24 07:56:20 +0100

    make nav bar and footer dynamic

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,90 +1,28 @@
 <template>
   <div id="app">
-    <NavBar />
     <div id="product-displayc">
         <CategoryBar />
         <TestPage />
     </div> 
     <ImgComponent />
-    <div id="ft-container">
-      <Footer v-for="(containers, index) in urlData" :key="containers.id" :propsUrlList="containers" :titleContent="titleData[index]" />
-    </div>
+
   </div>
 </template>
 
 <script>
 
-import NavBar from '../components/NavBar';
+
 import CategoryBar from '../components/CategoryBar';
 import ImgComponent from '../components/ImgComponent';
 import TestPage from '../components/TestPage';
-import Footer from '../components/FooterLinkComponent'
-export default {
-    name: 'App',
-  data(){
-    return{
-      titleData: ['LET US HELP YOU', 'ABOUT JUMIA', 'MAKE MONEY WITH JUMIA', 'JUMIA INTERNATIONAL'],
-      urlData: [
-        {
-          id: 1,
-          path: '#',
-          content: [
-            'Help Center', 
-            'Contact us',
-            'How to shop on Jumia?',
-            'Delivery options and timelines',
-            'How to return a product on Jumia?',
-            'Corporate and bulk purchases'
-            ]
-        },
-        {
-          id: 2,
-          path: '#',
-          content: [
-            'About us',
-            'Jumia careers',
-            'Jumia Express',
-            'Terms and Conditions',
-            'Privacy policy',
-            'Tech Week',
-            'Jumia Prime',
-            'Stay Safe'
-          ]
-        },
-        {
-          id: 3,
-          path: '#',
-          content: [
-            'Sell on Jumia',
-            'Become an Affiliate Partner',
-            'Become a Sales Consultant',
-            'Become a Jumia Vendor Service Provider',
-            'Become a Logistics Service Partner'
-          ]
-        },
-        {
-          id: 4,
-          path: '#',
-          content: [
-            'Algeria',
-            'Egypt',
-            'Ghana',
-            'Ivory Coast',
-            'Kenya',
-            'Morocco'
-          ]
-        }
-      ]
-    }
-  },
-  components: {
-    NavBar,
-    CategoryBar,
-    ImgComponent,
-    TestPage,
-    Footer
-  }
 
+export default {
+    name: 'Home',
+    components: {
+      CategoryBar,
+      ImgComponent,
+      TestPage
+    }
 }
 </script>
 
